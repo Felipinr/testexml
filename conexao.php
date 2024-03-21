74901463451ecba7dda8944dc1e5ec95aea4fca9
Gbpgustavo9 <159484180+Gbpgustavo9@users.noreply.github.com>
2024-03-21 16:51:11 -0300

    xml

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+    $servidor = 'db';
+    $usuario = 'root';
+    $senha = '12345';
+    $banco = 'db_xml';
+
+    $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+?>
---
+++ listar_alunos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista de alunos</h1>
+    <table 0>
+        <tr>
+            <td>id</td>
+            <td>nome</td>
+            <td>matricula</td>
+        </tr>
+        <?php
+        require_once "conexao.php";
+
+        $sql = "SELECT * FROM tb_aluno";
+
+        $resultados = mysqli_query($conexao, $sql);
+
+        if (mysqli_num_rows($resultados)) {
+            while ($linha = mysqli_fetch_array($resultados)) {
+                $id = $linha['id'];
+                $nome = $linha['nome'];
+                $matricula = $linha['matricula'];
+
+                echo "<tr>";
+                echo "<td>$id</td>";
+                echo "<td>$nome</td>";
+                echo "<td>$matricula</td>";
+                echo "</tr>";
+            }
+        }
+
+        ?>
+
+
+    </table>
+</body>
+</html>
